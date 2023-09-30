4364356189dd5e1a804e27e100b4f2229836c781
branavanj <104469463+branavanj@users.noreply.github.com>
2023-09-30 14:28:12 +0200

    calcul_moyenne.c

--- calcul_moyenne.c
+++ calcul_moyenne.c
@@ -7,10 +7,15 @@ int main() {
     printf("Combien de notes voulez-vous entrer ? ");
     scanf("%d", &nombreDeNotes);
     
-    // Vérifier si nombreDeNotes est valide (positif)
+    
+
+
+
+
+    
     if (nombreDeNotes <= 0) {
         printf("Le nombre de notes doit être supérieur à zéro.\n");
-        return 1;  // Quitter le programme avec un code d'erreur
+        return 1; 
     }
     
     for (int i = 1; i <= nombreDeNotes; i++) {
@@ -18,14 +23,13 @@ int main() {
         printf("Entrez la note #%d : ", i);
         scanf("%f", &note);
         
-        // Ajouter la note à la somme
+        
         somme += note;
     }
     
-    // Calculer la moyenne
     float moyenne = somme / nombreDeNotes;
     
-    // Afficher la moyenne
+    
     printf("La moyenne des notes est : %.2f\n", moyenne);
     
     return 0;
